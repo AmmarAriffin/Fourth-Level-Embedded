7f8350f401a10053e419093bd871c3d9a72a03e6
Ammar Ariffin <57162207+AmmarAriffin@users.noreply.github.com>
2024-08-16 20:00:33 +1200

    State Interface for State Pattern

---
+++ target/src/state.c
@@ -0,0 +1,57 @@
+
+
+// Inspiration from Fredy and here
+// https://www.adamtornhill.com/Patterns%20in%20C%202,%20STATE.pdf
+// Ammar Ariffin
+
+#include "state.h"
+#include "Fitness_Tracker.h"
+
+/* Generic Functions to default to when you dont declare on other*/
+static void defaultRightButPressed(FitnessTrackerPtr instance)
+{
+
+}
+
+static void defaultLeftButPressed(FitnessTrackerPtr instance)
+{
+    
+}
+
+static void defaultTopButPressed(FitnessTrackerPtr instance)
+{
+    
+}
+
+static void defaultBotButPressed(FitnessTrackerPtr instance)
+{
+    
+}
+
+static void defaultLeftSWPressed(FitnessTrackerPtr instance)
+{
+    
+}
+
+static void defaultupdateDisplay(FitnessTrackerPtr instance)
+{
+
+}
+
+static void defaultbotButLongPress(FitnessTrackerPtr instance)
+{
+
+}
+
+void initDefaultImplementation(StatePtr state)
+{
+    state->leftButPressed = defaultLeftButPressed;
+    state->rightButPressed = defaultRightButPressed;
+    state->topButPressed = defaultTopButPressed;
+    state->botButPressed = defaultBotButPressed;
+    state->leftSWPressed = defaultLeftSWPressed;
+    state->updateDisplay = defaultupdateDisplay;
+    state->botButLongPress = defaultbotButLongPress;
+}
+
+
---
+++ target/src/state.h
@@ -0,0 +1,33 @@
+// *******************************************************
+// 
+// state.h
+//
+// Implementing a State Pattern 
+// Ammar Ariffin
+// 
+// *******************************************************
+#ifndef STATE_H
+#define STATE_H
+
+// Forward declaration of State structure
+typedef struct State* StatePtr;
+
+#include "Fitness_Tracker.h"
+
+// Typedef for function *pointers
+typedef void (*ActionFunction)(FitnessTrackerPtr);
+
+// State structure
+struct State {
+    ActionFunction rightButPressed;
+    ActionFunction leftButPressed;
+    ActionFunction topButPressed;
+    ActionFunction botButPressed;
+    ActionFunction leftSWPressed;
+    ActionFunction updateDisplay;
+    ActionFunction botButLongPress;
+};
+
+void initDefaultImplementation(StatePtr state);
+
+#endif // STATE_H
